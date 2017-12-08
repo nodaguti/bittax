12ce90c05ded0003df6f3dbeeee7b109add54201
nodaguti <nodaguti@gmail.com>
2017-12-08 17:47:29 +0900

    Redesign provider panels

--- src/front/containers/SourceListToAdd.jsx
+++ src/front/containers/SourceListToAdd.jsx
@@ -11,55 +11,90 @@ import {
 import { requestOAuthIntegration } from '../redux/actions';
 import providers from '../providers';
 
-const mapStateToProps = () => ({});
+const mapStateToProps = (state) => ({
+  oauth: state.oauth,
+});
 
 class SourceListToAdd extends Component {
-  renderRow = ({ providersChunk, chunkId }) => {
-    const { history, dispatch } = this.props;
+  renderOAuthButton(id) {
+    const {
+      oauth,
+      dispatch,
+    } = this.props;
+    const isConnected = oauth.has(id);
 
+    return isConnected ? (
+      <Button block bsStyle="info">
+        OAuth 連携済
+      </Button>
+    ) : (
+      <Button
+        block
+        onClick={() => dispatch(requestOAuthIntegration({ provider: id }))}
+      >
+        OAuth 認証で連携
+      </Button>
+    );
+  }
+
+  renderAPIButton(id) {
+    const {
+      history,
+    } = this.props;
+
+    return (
+      <Button
+        block
+        onClick={() => history.push(`/integrate/api/${id}`)}
+      >
+        API Key を入力して連携
+      </Button>
+    );
+  }
+
+  renderCSVButton(id) {
+    const {
+      history,
+    } = this.props;
+
+    return (
+      <Button
+        block
+        onClick={() => history.push(`/source/add/${id}`)}
+      >
+        CSV ファイルからデータ読み込み
+      </Button>
+    );
+  }
+
+  renderPanel(provider) {
+    const { id, name } = provider;
+
+    return (
+      <Panel header={name} bsStyle="info">
+        {
+          !provider.oauth ? '' : this.renderOAuthButton(id)
+        }
+        {
+          !provider.api ? '' : this.renderAPIButton(id)
+        }
+        {
+          !provider.csv ? '' : this.renderCSVButton(id)
+        }
+      </Panel>
+    );
+  }
+
+  renderRow({ providersChunk, chunkId }) {
     return (
       <Row key={chunkId}>
         {
           providersChunk.map((provider) => {
-            const { id, name } = provider;
+            const { id } = provider;
 
             return (
               <Col xs={6} md={4} key={id}>
-                <Panel header={name} bsStyle="info">
-                  {
-                    !provider.oauth ? '' : (
-                      <Button
-                        onClick={
-                          () => dispatch(requestOAuthIntegration({ provider: id }))
-                        }
-                      >
-                        OAuth 認証で連携
-                      </Button>
-                    )
-                  }
-                  {
-                    !provider.api ? '' : (
-                      <Button
-                        onClick={
-                          () => history.push(`/integrate/api/${id}`)
-                        }
-                      >
-                        API Key を入力して連携
-                      </Button>
-                    )
-                  }
-                  {
-                    !provider.csv ? '' : (
-                      <Button
-                        onClick={
-                          () => history.push(`/source/add/${id}`)
-                        }
-                      >
-                        CSV ファイルからデータ読み込み
-                      </Button>
-                    )
-                  }
-                </Panel>
+                {this.renderPanel(provider)}
               </Col>
             );
           })
