cd5c03b2313d350b8d8b012081fa376a24816a58
nodaguti <nodaguti@gmail.com>
2017-12-11 00:08:07 +0900

    Unable to integrate another provider when a data fetch is in progress

--- src/front/redux/sagas/integration.js
+++ src/front/redux/sagas/integration.js
@@ -1,4 +1,4 @@
-import { fork, take, put } from 'redux-saga/effects';
+import { fork, take, put, takeEvery } from 'redux-saga/effects';
 import {
   REQUEST_OAUTH_INTEGRATION,
   OAUTH_TOKEN_FETCHED,
@@ -6,25 +6,26 @@ import {
   appendNotification,
 } from '../actions';
 
-function* integrateViaOAuth() {
+function* callFetchTokenAndTransactions({ payload: { provider } }) {
+  // Wait until the expecting token has been fetched
   while (true) {
-    const { payload: { provider } } = yield take(REQUEST_OAUTH_INTEGRATION);
+    const { payload: { provider: _provider } } = yield take(OAUTH_TOKEN_FETCHED);
 
-    while (true) {
-      const { payload: { provider: _provider } } = yield take(OAUTH_TOKEN_FETCHED);
-
-      if (_provider === provider) {
-        break;
-      }
+    if (_provider === provider) {
+      break;
     }
-
-    yield put(appendNotification({
-      id: `${provider}-integration-completed`,
-      message: `${provider} の OAuth 連携が完了しました．取引データの取得完了まで今しばらくお待ちください．`,
-    }));
-
-    yield put(fetchTradesOfAllPairs({ provider }));
   }
+
+  yield put(appendNotification({
+    id: `${provider}-integration-completed`,
+    message: `${provider} の OAuth 連携が完了しました．取引データの取得完了まで今しばらくお待ちください．`,
+  }));
+
+  yield put(fetchTradesOfAllPairs({ provider }));
+}
+
+function* integrateViaOAuth() {
+  yield takeEvery(REQUEST_OAUTH_INTEGRATION, callFetchTokenAndTransactions);
 }
 
 export default function* integrationSaga() {
