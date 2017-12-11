0b7bb53296c1ff9012ff1e903157b36f87608833
nodaguti <nodaguti@gmail.com>
2017-12-12 00:18:50 +0900

    Add FormattedText

---
+++ src/front/components/FormattedText/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+const FormattedText = (props) => (
+  <FormattedMessage {...props}>
+    {(text) => text}
+  </FormattedMessage>
+);
+
+export default FormattedText;
